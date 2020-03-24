6bd81f6a7b6c0722dc78f1a56741eadee228e70e
jzamora5 <jzamora_5@yahoo.com>
2020-03-24 13:15:27 +0000

    Test Quotes

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Manifest to modify SSH config file
 file_line { 'Turn off passwd auth':
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
   match  => '    PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/holberton',
   match  => '    IdentityFile',
