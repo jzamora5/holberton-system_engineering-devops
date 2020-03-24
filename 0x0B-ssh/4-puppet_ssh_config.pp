e7a2a460422a585668671d2e33f55c3e3b53864f
jzamora5 <jzamora_5@yahoo.com>
2020-03-24 15:54:25 +0000

    Remove a comma

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,11 +2,10 @@
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+  line   => '    PasswordAuthentication no'
 }
-
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/holberton',
+  line   => '    IdentityFile ~/.ssh/holberton'
 }
