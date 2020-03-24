ce1a5c1ad5389835530438165c19ca5db51a3d63
jzamora5 <jzamora_5@yahoo.com>
2020-03-24 13:35:28 +0000

    Change order of commands

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Manifest to modify SSH config file
 exec { 'echo':
-  command => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
   path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
   returns => [0,1],
 }
