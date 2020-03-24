3407a42af5493c7608c303fee85d138d459dbdab
jzamora5 <jzamora_5@yahoo.com>
2020-03-24 13:31:10 +0000

    ADD More Paths

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Manifest to modify SSH config file
 exec { 'echo':
   command => 'echo "    PasswordAuthentication no\n    IdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
-  path    => '/bin',
+  path    => 'usr/bin:/bin',
   returns => [0,1],
 }
