1426b1bd67027381b618931baa9f06ee713e1d77
jzamora5 <jzamora_5@yahoo.com>
2020-04-23 14:20:42 +0000

    Create Database User

---
+++ 0x14-mysql/1-let_us_in.sql
@@ -0,0 +1,6 @@
+-- Key for checker goes in:
+--.ssh/authorized_keys
+-- SQL Queries both servers
+CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
