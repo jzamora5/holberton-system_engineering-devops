70b4d7044fe953afa98f4ca06af200e3609af7c7
jzamora5 <jzamora_5@yahoo.com>
2020-05-04 15:52:07 +0000

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Dictionary of list of dictionaries  """
+
+if __name__ == "__main__":
+    from requests import get
+    import json
+
+    url_user = "https://jsonplaceholder.typicode.com/users"
+    url_todo = "https://jsonplaceholder.typicode.com/todos"
+
+    r_user = get(url_user)
+    r_todo = get(url_todo)
+
+    try:
+        js_user = r_user.json()
+        js_todo = r_todo.json()
+
+    except ValueError:
+        print("Not a valid JSON")
+
+    if js_user and js_todo:
+        data = {}
+        user_names = {}
+        for user in js_user:
+            USER_ID = user.get('id')
+            USERNAME = user.get('username')
+            data[USER_ID] = []
+            user_names[USER_ID] = USERNAME
+
+        for todo in js_todo:
+            TASK_COMPLETED_STATUS = todo.get("completed")
+            TASK_TITLE = todo.get('title')
+            user_id = todo.get("userId")
+            dic = {"task": TASK_TITLE,
+                   "completed": TASK_COMPLETED_STATUS,
+                   "username": user_names.get(user_id)}
+
+            if data.get(user_id) is not None:
+                data.get(user_id).append(dic)
+
+        with open('todo_all_employees.json', 'w', newline='') as jsonfile:
+            json.dump(data, jsonfile)
