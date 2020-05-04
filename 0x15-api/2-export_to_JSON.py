104b274bc7efe1fc40a1ae334ea8e06d635b1ee6
jzamora5 <jzamora_5@yahoo.com>
2020-05-04 15:16:25 +0000

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Export to JSON  """
+
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv, exit
+    import json
+
+    try:
+        id = argv[1]
+        is_int = int(id)
+    except:
+        exit()
+
+    url_user = "https://jsonplaceholder.typicode.com/users?id=" + id
+    url_todo = "https://jsonplaceholder.typicode.com/todos?userId=" + id
+
+    r_user = get(url_user)
+    r_todo = get(url_todo)
+
+    try:
+        js_user = r_user.json()
+        js_todo = r_todo.json()
+
+    except ValueError:
+        print("Not a valid JSON")
+
+    if js_user and js_todo:
+        USER_ID = id
+        USERNAME = js_user[0].get('username')
+
+        js_list = []
+        for todo in js_todo:
+            TASK_COMPLETED_STATUS = todo.get("completed")
+            TASK_TITLE = todo.get('title')
+
+            dic = {"task": TASK_TITLE,
+                   "completed": TASK_COMPLETED_STATUS,
+                   "username": USERNAME}
+
+            js_list.append(dic)
+
+        data = {USER_ID: js_list}
+
+        with open(id + '.json', 'w', newline='') as jsonfile:
+            json.dump(data, jsonfile)
