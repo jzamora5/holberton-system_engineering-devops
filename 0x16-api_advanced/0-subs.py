22836e6cba0120ec548d5757bf89c57c9d54e60c
jzamora5 <jzamora_5@yahoo.com>
2020-05-07 14:10:33 +0000

    How Many Subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" How many subs? """
+
+
+def number_of_subscribers(subreddit):
+    """ Returns subscriber count of subreddit or 0 """
+    from requests import get
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    r = get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return 0
+
+    try:
+        js = r.json()
+
+    except ValueError:
+        return 0
+
+    data = js.get("data")
+
+    if data:
+        sub_count = data.get("subscribers")
+        return sub_count
+
+    return 0
