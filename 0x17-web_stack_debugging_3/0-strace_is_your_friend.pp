261efb59512c5f0865fc4fc54b59274a06ecaa2e
jzamora5 <jzamora_5@yahoo.com>
2020-05-19 15:28:47 +0000

    Fix sed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,9 @@
 # Web Stack debugging fix typo in config file
 exec { 'fix_typo':
-  environment => ['DIR=/var/www/html/wp-settings.php'],
-  command     => 'sudo sed -i "s/class-wp-locale.phpp;/class-wp-locale.php;/" $DIR',
+  environment => ['DIR=/var/www/html/wp-settings.php',
+                  "L=require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );",
+                  "LF=require_once( ABSPATH . WPINC . '/class-wp-locale.php' );"],
+  command     => 'sudo sed -i "s/$L;/$LF;/" $DIR',
   path        => ['/usr/bin', '/bin'],
   returns     => [0,1]
 }
