68a31016f0b92090e5ea1db5f4a7452882088f29
jzamora5 <jzamora_5@yahoo.com>
2020-05-26 15:04:50 +0000

    Add sudo to nginx restart

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,7 +3,7 @@ exec { 'fix--for-nginx':
   environment => ['DIR=/etc/default/nginx',
                   'OLD=ULIMIT="-n 15"',
                   'NEW=ULIMIT="-n 15000"'],
-  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; service nginx restart',
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo service nginx restart',
   path        => ['/usr/bin', '/bin'],
   returns     => [0, 1]
 }
